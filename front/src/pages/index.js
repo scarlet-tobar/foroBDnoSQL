7d8da944bf56f374aaf81f2855a3681f6d56f3d9
Diego Barros <diego.barros@alumnos.ucn.cl>
2023-07-20 20:46:21 -0400

    feat(index): update index.js

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,7 +1,5 @@
-// index.js
-
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Grid } from '@mui/material';
+import { Container, Typography, Grid, Button } from '@mui/material';
 import { gql, useQuery } from '@apollo/client';
 import Navbar from '@/components/navbar';
 import PostContainer from '@/components/PostContainer';
@@ -47,12 +45,14 @@ const IndexPage = () => {
   const [userEmail, setUserEmail] = useState('');
   const [communities, setCommunities] = useState([]);
   const [sortedPosts, setSortedPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Estado para almacenar la página actual
+  const postsPerPage = 5; // Mostraremos 5 posts por página
 
   useEffect(() => {
     // Obtener el email del usuario logeado desde el localStorage
     const email = localStorage.getItem('email');
-    if (!email){
-      window.location.href= '/login';
+    if (!email) {
+      window.location.href = '/login';
     }
     setUserEmail(email);
   }, []);
@@ -81,6 +81,26 @@ const IndexPage = () => {
   if (communityLoading || postsLoading) return <p>Loading...</p>;
   if (communityError || postsError) return <p>Error: {communityError?.message || postsError?.message}</p>;
 
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  // Calcular la cantidad total de páginas
+  const totalPages = Math.ceil(sortedPosts.length / postsPerPage);
+
+  // Lógica para mostrar solo dos botones de página
+  const showPrevButton = currentPage > 1;
+  const showNextButton = currentPage < totalPages;
+
+  // Lógica para obtener los posts de la página actual
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = sortedPosts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
     <div>
       <Navbar />
@@ -96,19 +116,31 @@ const IndexPage = () => {
       >
         <Grid container spacing={2}>
           <Grid item xs={9}>
-            {sortedPosts.map((post) => (
+            {currentPosts.map((post) => (
               <PostContainer key={post.idPrimary} post={post} />
             ))}
+            <Grid container justifyContent="center" spacing={2}>
+              <Grid item>
+                {showPrevButton && (
+                  <Button variant="contained" onClick={handlePrevPage}>Anterior</Button>
+                )}
+              </Grid>
+              <Grid item>
+                {showNextButton && (
+                  <Button variant="contained" onClick={handleNextPage}>Siguiente</Button>
+                )}
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item xs={3}>
-          <Grid container justifyContent="space-between" spacing={2}>
-          <Grid item>
-            <CreatePostPopup userEmail={userEmail} />
-          </Grid>
-          <Grid item>
-            <CreateCommunity userEmail={userEmail} />
-          </Grid>
-        </Grid>
+            <Grid container justifyContent="space-between" spacing={2}>
+              <Grid item>
+                <CreatePostPopup userEmail={userEmail} />
+              </Grid>
+              <Grid item>
+                <CreateCommunity userEmail={userEmail} />
+              </Grid>
+            </Grid>
             <FriendList />
             <CommunityList communities={communities} />
           </Grid>
